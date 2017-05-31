749df4122b36397a227b6afde66a1ff0908ca1f2
1017824 <1017824@50HW002787>
2017-05-31 09:59:29 -0500

    Hasta Punto 8 del Lab02

---
+++ Divisas2/Divisas2/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,13 @@
+﻿using Divisas2.ViewModels;
+
+namespace Divisas2.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main;
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
